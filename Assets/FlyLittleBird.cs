72afea9fe34eba1303ccb0474f90431e52e7f214
Alibek Olimov <2110101@newuzbekistanuniversity.uz>
2024-09-18 17:47:45 +0500

    my gameeee

---
+++ Assets/FlyLittleBird.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyLittleBird : MonoBehaviour
+{
+
+    public float velocity = 5;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
